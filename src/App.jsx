611aa67b5e80b0bf5edb3f94721039068112a4d1
Somya Bhalani <somyabhalani@gmail.com>
2026-06-12 15:15:35 +0530

    Remove legacy carbonix files from ananta-green repo to fix Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,6 @@ import ScrollToTop from './components/ScrollToTop'
 import PageTransition from './components/PageTransition'
 import Home from './pages/Home'
 import CarbonJournal from './pages/CarbonJournal'
-import CarbonixMap from './pages/carbonix/CarbonixMap'
-import CarbonixProfile from './pages/carbonix/CarbonixProfile'
-import CarbonixVerifying from './pages/carbonix/CarbonixVerifying'
-import CarbonixCertificate from './pages/carbonix/CarbonixCertificate'
 import './App.css'
 
 function App() {
@@ -38,11 +34,6 @@ function App() {
             <Route path="/" element={<PageTransition><Home /></PageTransition>} />
             <Route path="/green/journal/carbon" element={<PageTransition><CarbonJournal /></PageTransition>} />
             
-            {/* Carbonix Multi-Page Flow */}
-            <Route path="/carbonix/map" element={<PageTransition><CarbonixMap /></PageTransition>} />
-            <Route path="/carbonix/profile" element={<PageTransition><CarbonixProfile /></PageTransition>} />
-            <Route path="/carbonix/verifying" element={<PageTransition><CarbonixVerifying /></PageTransition>} />
-            <Route path="/carbonix/certificate" element={<PageTransition><CarbonixCertificate /></PageTransition>} />
           </Routes>
         </AnimatePresence>
       </SmoothScroll>
